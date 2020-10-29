786ab1b6432be6a301370d3572601d2e3a0e1917
Nao-fuji <72326957+Nao-fuji@users.noreply.github.com>
2020-10-29 19:14:29 +0900

    Revert "商品出品機能の実装"

--- Gemfile
+++ Gemfile
@@ -64,8 +64,3 @@ end
 gem 'devise'
 
 gem 'pry-rails'
-
-gem 'active_hash'
-
-gem 'mini_magick'
-gem 'image_processing','~>1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,4 @@
 class ItemsController < ApplicationController
   def index
   end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def item_params
-    params.require(:item).permit(:name, :price, :category_id, :status_id, :explain, :shipping_address_id, :shipping_date_id, :delivery_charge_id, :image).merge(user_id: current_user.id)
-  end
 end
