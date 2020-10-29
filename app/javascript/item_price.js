45d98eaf864d759d2fd6ee5d840f2a1083d6ed4c
Nao-fuji <fujitaninaoya@MacBook-Pro.local>
2020-10-29 18:03:26 +0900

    商品出品機能の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input",()=> {
+    const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = inputValue / 10
+  const profitPrice = document.getElementById("profit");
+  profitPrice.innerHTML = inputValue - inputValue /10
+    
+  });
+});
