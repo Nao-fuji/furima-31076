ce55cd7e1a1c6aa1c2e53104d5feaa6eaa6f6165
Nao-fuji <fujitaninaoya@MacBook-Pro.local>
2020-10-30 11:06:38 +0900

    商品出品機能（修正）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
-  validates :name, :price, :category, :status, :explain, :shipping_address, :shipping_date, :delivery_charge, :image, presence: true
-  validates :category_id, :status_id, :shipping_date_id, :shipping_address_id, :delivery_charge_id, numericality: { other_than: 1 }
-
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id, :status_id, :shipping_date_id, :shipping_address_id, :delivery_charge_id
+  end
+  validates :name, :price, :explain, :image, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: '価格の範囲は¥300~¥9,999,999の間で設定してください' }
 
   belongs_to :user
