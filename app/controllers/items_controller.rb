843bb6bdc369c422bd0b52b19383deb9ced87713
Nao-fuji <fujitaninaoya@MacBook-Pro.local>
2020-10-31 17:45:55 +0900

    商品情報編集機能(before_action使用)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item,only:[:show,:edit,:update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -17,16 +18,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -38,5 +36,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :price, :category_id, :status_id, :explain, :shipping_address_id, :shipping_date_id, :delivery_charge_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
